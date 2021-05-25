9ca2cdb84f6951441739d3359dea8ab5b6a0853f
lgcarlinf <lgcarlinf@unac.edu.pe>
2021-05-25 13:03:18 -0500

    proyecto free

---
+++ imaxventas/main.js
@@ -0,0 +1,7 @@
+let enlacesHeader = document.querySelectorAll(".enlaces-header")[0];
+
+
+document.querySelectorAll(".hamburguer")[0].addEventListener("click",() =>{
+
+    enlacesHeader.classList.toggle("menudos");
+})
